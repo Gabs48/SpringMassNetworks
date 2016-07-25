6b8195b2b019dbb4c918a9f78d7fce6adbe8ea6b
Gabriel Urbain <gabriel.urbain@gmail.com>
2016-07-25 17:19:53 +0200

    Replace experiment function with a class

--- hpc_batch.py
+++ hpc_batch.py
@@ -12,39 +12,64 @@ import platform
 import sys
 import datetime
 
-def experiment(fileName_="CMA", folderName_="Data", noNodes_=20, spring_=100, noNeighbours_=3, plot_=False, \
-	simTimeStep_=0.005, simTime_=20, perfMetr_="dist", controlPlot_=False, maxIter_=5000, maxOmega=10, \
-	maxAmplitude=0.25):
-	"""Start a standard experiment"""
 
-	env = HardEnvironment()
-	morph = SpringMorphology(noNodes=noNodes_ , spring=spring_, noNeighbours=noNeighbours_, environment=env)
-	control = SineControl(morph)
-	robot = Robot(morph,control)
+class Experiment(object):
+	"""Class to perform standard experiments"""
 
-	plotter = Plotter(plot=False);
-	simulenv = SimulationEnvironment(timeStep=simTimeStep_, simulationLength=int(simTime_/simTimeStep_), \
-	plot=plotter, perfMetr=perfMetr_, controlPlot=controlPlot_)
+	def __init__(self, fileName_="CMA", folderName_="Data", noNodes_=20, spring_=100, noNeighbours_=3, plot_=False, \
+		simTimeStep_=0.005, simTime_=20, perfMetr_="dist", controlPlot_=False, maxIter_=5000, maxOmega_=10, \
+		maxAmplitude_=0.25):
+		"""Initialize the variables lists"""
 
-	trainscheme = TrainingScheme()
-	trainscheme.createTrainVariable("omega", 0, maxOmega)
-	trainscheme.createTrainVariable("phase", 0, 2 * np.pi)
-	trainscheme.createTrainVariable("amplitude", 0, maxAmplitude)
+		self.fileName = fileName_
+		self.folderName = folderName_
+		self.noNodes = noNodes_
+		self.spring = spring_
+		self.noNeighbours = noNeighbours_
+		self.plot = plot_
+		self.simTimeStep = simTimeStep_
+		self.simTime = simTime_
+		self.perfMetr = perfMetr_
+		self.controlPlot = controlPlot_
+		self.maxIter = maxIter_
+		self.maxOmega = maxOmega_
+		self.maxAmplitude = maxAmplitude_
 
-	saver = Save(None, fileName_, folderName_)
-	train = CMATraining(trainscheme, robot, simulenv, saver=saver, maxIter=maxIter_)
+	def run(self):
+		"""Run the experiment"""
 
-	param, score, t_tot = train.run() # perform optimization
+		# Init environment
+		env = HardEnvironment()
+		morph = SpringMorphology(noNodes=self.noNodes , spring=self.spring, noNeighbours=self.noNeighbours, environment=env)
+		control = SineControl(morph)
+		robot = Robot(morph, control)
 
-	comm = MPI.COMM_WORLD
-	rank = comm.Get_rank()
-	size = comm.Get_size()
-	machine = platform.node()
-	print("-- " + machine + " (" + str(rank+1) + "/" + str(size) + ")" + \
-		" -- Total training time: " + "{:.1f}".format(t_tot)  + " s")
-	print("-- " + machine + " (" + str(rank+1) + "/" + str(size) + ")" + \
-		" -- Best score (" + perfMetr_ + "): {:.2f}".format(score)  + "\n")
-	train.save()
+		plotter = Plotter(plot=False);
+		simulenv = SimulationEnvironment(timeStep=self.simTimeStep, simulationLength=int(self.simTime/self.simTimeStep), \
+		plot=plotter, perfMetr=self.perfMetr, controlPlot=self.controlPlot)
+
+		trainscheme = TrainingScheme()
+		trainscheme.createTrainVariable("omega", 0, self.maxOmega)
+		trainscheme.createTrainVariable("phase", 0, 2 * np.pi)
+		trainscheme.createTrainVariable("amplitude", 0, self.maxAmplitude)
+
+		saver = Save(None, self.fileName, self.folderName)
+		train = CMATraining(trainscheme, robot, simulenv, saver=saver, maxIter=self.maxIter)
+
+		# Perform optimization
+		param, score, t_tot = train.run() 
+
+		# Print and save results
+		comm = MPI.COMM_WORLD
+		rank = comm.Get_rank()
+		size = comm.Get_size()
+		machine = platform.node()
+		print("-- " + machine + " (" + str(rank+1) + "/" + str(size) + ")" + \
+			" -- Total training time: " + "{:.1f}".format(t_tot)  + " s")
+		print("-- " + machine + " (" + str(rank+1) + "/" + str(size) + ")" + \
+			" -- Best score (" + self.perfMetr + "): {:.2f}".format(score)  + "\n")
+		print(" == Experiment finished with the following parameters == \n\n  " + str(self.__dict__) + "\n")
+		train.save()
 
 
 def createParetoVal():
@@ -60,6 +85,7 @@ def createParetoVal():
 
 	return liste
 
+
 if __name__ == "__main__":
 	"""Start the experiment function with different parameters"""
 
@@ -73,7 +99,7 @@ if __name__ == "__main__":
 	machine = platform.node()
 
 	# Print machine info	
-	print("\n == Robot Training Experiment with Multiprocessing == ")
+	print("\n == Initializing Robot Training Experiment == ")
 	print("\n  Date: " + str(datetime.datetime.now()))
 	print("  Machine: " + machine + " (" + str(rank+1) + "/" + str(size) + ")")
 	print("  OS: " + str(platform.system()) + " version " + str(platform.release()))
@@ -90,9 +116,9 @@ if __name__ == "__main__":
 			arg_list = createParetoVal()
 			fileName = "Machine-" + str(rank)
 			n_iteration = int(math.ceil(len(arg_list)/float(size)))
-			print("  Running " +  str(len(arg_list)) + " experiments on " + str(size) + \
+			print(" == Running " +  str(len(arg_list)) + " experiments on " + str(size) + \
 				" processors: " + str(n_iteration) + " optimizations expected in approximately " + \
-				"{:.2f} hours \n".format(float(simTime) / 20 * trainingIt * n_iteration / 3600))
+				"{:.2f} hours \n".format(float(simTime) / 20 * trainingIt * n_iteration / 3600) + " == ")
 
 			# Simulate multiple time if the number of cores does not correspond to number of points
 			for i in range(n_iteration):
@@ -101,10 +127,15 @@ if __name__ == "__main__":
 					print("-- " + machine + " (" + str(rank+1) + "/" + str(size) + ")" + \
 						" -- Experiment " + str(index+1) + " with Omega=" + \
 						str(arg_list[index][0]) + " and Amplitude=" + str(arg_list[index][1]))
-					experiment(fileName_=fileName, folderName_="Pareto", maxOmega=arg_list[index][0], \
-						simTime_=simTime, maxIter_=trainingIt,  maxAmplitude=arg_list[index][1])
-
+					e = Experiment(fileName_=fileName, folderName_="Pareto", maxOmega_=arg_list[index][0], \
+						simTime_=simTime, maxIter_=trainingIt,  maxAmplitude_=arg_list[index][1])
+					e.run()
 	else:
 		# Simulate a pool of CMA otpimization with the same arguments
+		print(" == Running " +  str(size) + " experiments on " + str(size) + \
+			" processors: 1 optimization expected in approximately " + \
+			"{:.2f} hours == \n".format(float(simTime) / 20 * trainingIt / 3600))
 		fileName = "Machine-" + str(rank)
-		experiment(fileName_=fileName, folderName_="CMA")
+
+		e = Experiment(fileName_=fileName, folderName_="CMA", simTime_=simTime, maxIter_=trainingIt)
+		e.run()
