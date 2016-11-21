8e969cf57ae8ba55b54049dc33f881e6293e8562
Gabriel Urbain <gabriel.urbain@gmail.com>
2016-11-21 17:45:53 +0100

    Test new metrics for pareto

--- roboTraining/experiment.py
+++ roboTraining/experiment.py
@@ -176,10 +176,10 @@ def createRefPowerParetoVal():
 	liste = []
 	# power =  np.linspace(100, 20000, num=10).tolist()
 	power =  np.array([100, 500, 1000, 2500, 5000, 7500, 10000, 12500, 15000, 17500]).tolist()
-	freq =  []
+	freq =  [3]
 	# Average with 5 values
-	for i in range(1, 5):
-		freq.extend(range(1, 5))
+	# for i in range(1, 5):
+	# 	freq.extend(range(1, 5))
 
 	for f in freq:
 		for p in power:
@@ -192,10 +192,10 @@ def createRefDistParetoVal():
 
 	liste = []
 	dist =  np.linspace(10, 300, num=10).tolist()
-	freq =  []
-	# Average with 5 values
-	for i in range(1, 5):
-		freq.extend(range(1, 5))
+	freq =  [3]
+	# # Average with 5 values
+	# for i in range(1, 5):
+	# 	freq.extend(range(1, 5))
 
 	for f in freq:
 		for d in dist:
